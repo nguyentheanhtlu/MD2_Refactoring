1a99578b94bfb4e5bdeaf340183e9f03bbfcce45
nguyentheanhtlu <theanh28022003@gmail.com>
2022-10-11 08:40:07 +0700

    Refactoring

--- FizzBuzz.ts
+++ FizzBuzz.ts
@@ -1,12 +1,27 @@
 export class FizzBuzz {
     message: string;
 
+//     constructor(n: number) {
+//         if (n % 3 == 0 && n % 5 == 0) {
+//             this.message = 'FizzBuzz';
+//         } else if (n % 5 == 0) {
+//             this.message = 'Fizz';
+//         } else if(n % 3 == 0) {
+//             this.message = 'Buzz';
+//         } else {
+//             this.message = n + ''
+//         }
+//     }
+// }
     constructor(n: number) {
-        if (n % 3 == 0 && n % 5 == 0) {
+        let isFizz = n % 5 == 0;
+        let isBuzz = n % 3 == 0;
+
+        if (isBuzz && isBuzz) {
             this.message = 'FizzBuzz';
-        } else if (n % 5 == 0) {
+        } else if (isFizz) {
             this.message = 'Fizz';
-        } else if(n % 3 == 0) {
+        } else if(isBuzz) {
             this.message = 'Buzz';
         } else {
             this.message = n + ''
